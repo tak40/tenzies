72dd8b26347f6c7c75483c6ef95058be283bd282
Tak <takeshiseodev@gmail.com>
2023-11-29 15:59:32 -0500

    initial deployment

---
+++ components/Die.jsx
@@ -0,0 +1,11 @@
+/** @format */
+
+function Die(props) {
+    return (
+        <div className="die">
+            <h2 className="die-num">{props.value}</h2>
+        </div>
+    )
+}
+
+export default Die
---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+/** @format */
+import Die from '/components/Die.jsx'
+
+function App() {
+    return (
+        <main>
+            <div className='dice-container'>
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+            </div>
+        </main>
+    )
+}
+
+export default App
