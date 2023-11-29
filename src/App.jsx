19313a80e4fc70c920d9653dcff1d6d47aac09f2
Tak <takeshiseodev@gmail.com>
2023-11-29 16:05:41 -0500

    update: added allNewDice function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 /** @format */
 import Die from '/components/Die.jsx'
 
+function allNewDice() {
+    const randomNumbers = []
+    for (let i = 0; i < 10; i++) {
+        randomNumbers.push(Math.floor(Math.random() * 6) + 1)
+    }
+    return randomNumbers
+}
+
+console.log(allNewDice())
+
 function App() {
     return (
         <main>
-            <div className='dice-container'>
+            <div className="dice-container">
                 <Die value={1} />
                 <Die value={1} />
                 <Die value={1} />
