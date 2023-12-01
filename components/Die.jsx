a4f701f8055f7619cc057b35c203609b0be63b75
Tak <takeshiseodev@gmail.com>
2023-11-30 22:51:15 -0500

    Feature Update: Implement Dice Hold Functionality and Enhance CSS Structure

--- components/Die.jsx
+++ components/Die.jsx
@@ -1,8 +1,10 @@
 /** @format */
 
 function Die(props) {
+    const dieStyle = { backgroundColor: props.isHeld ? '#59E391' : 'white' }
+
     return (
-        <div className="die">
+        <div className="die" style={dieStyle} onClick={props.holdDice}>
             <h2 className="die-num">{props.value}</h2>
         </div>
     )
--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,65 @@
 /** @format */
+import { useState } from 'react'
+import { nanoid } from 'nanoid'
 import Die from '/components/Die.jsx'
 
 function App() {
+    const [dice, setDice] = useState(allNewDice())
+
     function allNewDice() {
-        const randomNumbers = []
+        const newDice = []
         for (let i = 0; i < 10; i++) {
-            randomNumbers.push(Math.floor(Math.random() * 6) + 1)
+            newDice.push({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid(),
+            })
         }
-        return randomNumbers
+        return newDice
     }
+
+    function holdDice(id) {
+        setDice(prevDice =>
+            prevDice.map(die => {
+                if (die.id === id) {
+                    return { ...die, isHeld: !die.isHeld }
+                }
+                return die
+            })
+        )
+    }
+
+    function rollNewDice() {
+        setDice(prevDice =>
+            prevDice.map(die => {
+                return die.isHeld
+                    ? die
+                    : {
+                          ...die,
+                          value: Math.ceil(Math.random() * 6),
+                      }
+            })
+        )
+    }
+
+    const diceElements = dice.map(die => (
+        <Die
+            key={die.id}
+            value={die.value}
+            isHeld={die.isHeld}
+            holdDice={() => holdDice(die.id)}
+        />
+    ))
+
     return (
         <main>
-            <div className="dice-container">
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-            </div>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">
+                Roll until all dice are the same. Click each die to freeze it at
+                its current value between rolls.
+            </p>
+            <div className="dice-container">{diceElements}</div>
+            <button onClick={rollNewDice}>Roll</button>
         </main>
     )
 }
