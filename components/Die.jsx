e93e862280cb00024d101b88351802ffa56a3029
Tak <takeshiseodev@gmail.com>
2024-01-06 22:10:34 -0500

    Dice Design Overhaul & UI Fine Tuning (Classic Theme)

--- components/Die.jsx
+++ components/Die.jsx
@@ -1,11 +1,58 @@
 /** @format */
 
-function Die(props) {
-    const dieStyle = { backgroundColor: props.isHeld ? '#59E391' : 'white' }
+// function Die(props) {
+//     const dieStyle = { backgroundColor: props.isHeld ? '#59E391' : 'white' }
+
+//     return (
+//         <div className="die" style={dieStyle} onClick={props.holdDice}>
+//             <h2 className="die-num">{props.value}</h2>
+//         </div>
+//     )
+// }
+
+function Die({ value, isHeld, holdDice }) {
+    const renderDots = () => {
+        // Define what the dot pattern looks like for each value
+        const patterns = {
+            1: [<div className="dot center-dot"></div>],
+            2: [
+                <div className="dot top-left"></div>,
+                <div className="dot bottom-right"></div>,
+            ],
+            3: [
+                <div className="dot top-left"></div>,
+                <div className="dot center-dot"></div>,
+                <div className="dot bottom-right"></div>,
+            ],
+            4: [
+                <div className="dot top-left"></div>,
+                <div className="dot top-right"></div>,
+                <div className="dot bottom-left"></div>,
+                <div className="dot bottom-right"></div>,
+            ],
+            5: [
+                <div className="dot top-left"></div>,
+                <div className="dot top-right"></div>,
+                <div className="dot center-dot"></div>,
+                <div className="dot bottom-left"></div>,
+                <div className="dot bottom-right"></div>,
+            ],
+            6: [
+                <div className="dot top-left"></div>,
+                <div className="dot top-right"></div>,
+                <div className="dot middle-left"></div>,
+                <div className="dot middle-right"></div>,
+                <div className="dot bottom-left"></div>,
+                <div className="dot bottom-right"></div>,
+            ],
+        }
+        // Return the pattern that matches the current value
+        return patterns[value]
+    }
 
     return (
-        <div className="die" style={dieStyle} onClick={props.holdDice}>
-            <h2 className="die-num">{props.value}</h2>
+        <div className={`die ${isHeld ? 'held' : ''}`} onClick={holdDice}>
+            {renderDots()}
         </div>
     )
 }
